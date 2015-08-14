4c26c0d3224f9bc1eaf302fe91d707418c447f9e
Aleksey Motovilov <lehaqs@gmail.com>
2015-08-14 17:56:25 +0300

    Compile assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,21 +16,41 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		/**
-		 * Clean dist files
+		 * Clean dist files.
 		 */
 		clean: [
 			'web/app/themes/leham/assets/js/dist/*.js',
 			'web/app/themes/leham/assets/css/dist/*.css',
-		]
+		],
 
+		/**
+		 * Add hash to all assets.
+		 */
+		hash: {
+			options: {
+				hashLength: 8,
+				hashFunction: function(source, encoding) {
+					return require('crypto').createHash('sha1').update(source, encoding).digest('hex');
+				}
+			},
+			main_js: {
+				src: 'web/app/themes/leham/assets/js/compiled/main.js',
+				dest: 'web/app/themes/leham/assets/js/dist/'
+			},
+			main_css: {
+				src: 'web/app/themes/leham/assets/css/compiled/main.css',
+				dest: 'web/app/themes/leham/assets/css/dist/'
+			}
+		}
 	});
 
-	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-hash');
+	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-	grunt.registerTask('default', ['clean']);
+	grunt.registerTask('test', ['clean', 'hash']);
+	grunt.registerTask('default', ['test']);
 };
--- web/app/themes/leham/index.php
+++ web/app/themes/leham/index.php
@@ -3,7 +3,9 @@
 	<?php if ( have_posts() ) : ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
+			<?php the_post_thumbnail(); ?>
 			<?php the_title(); ?>
+			<?php the_excerpt(); ?>
 		<?php endwhile; ?>
 
 	<?php endif; ?>

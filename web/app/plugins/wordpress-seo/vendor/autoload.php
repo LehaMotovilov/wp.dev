2320037b4df3bedef13c5578ee009b4f7c0ad3fc
Aleksey Motovilov <lehaqs@gmail.com>
2016-04-22 10:39:07 +0300

    Update SEO plugin.

--- web/app/plugins/wordpress-seo/vendor/autoload.php
+++ web/app/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit8bac1eb5c885ec9130bd6cbbf70e8643::getLoader();
+return ComposerAutoloaderInit8f412f0bce78c2c2ae84a1163b773a57::getLoader();
--- web/app/plugins/wordpress-seo/vendor/autoload_52.php
+++ web/app/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit5388019ee4956e84a7af4367c36e4bf9::getLoader();
+return ComposerAutoloaderInitd63c710cea090f99b7d88fe03d096454::getLoader();
